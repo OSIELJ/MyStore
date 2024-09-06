3fb54e17f19b20ab8c24841a06522766e82aaac1
OSIELJ <osieljuniormartins@gmail.com>
2024-09-06 10:36:14 -0300

    Configurações iniciais.
    Adição de cores, dependências , fontes e criações de pacotes.

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
+        classpath("com.google.gms:google-services:4.4.2")
+
+        val nav_version = "2.8.0"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+}
