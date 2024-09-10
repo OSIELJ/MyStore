4b53d023efea2de08378a5c658020f25a87d4b80
OSIELJ <osieljuniormartins@gmail.com>
2024-09-10 08:03:31 -0300

    Criação das classes  fragments.

---
+++ app/src/main/java/com/example/mystore/fragments/loginRegister/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.example.mystore.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.mystore.R
+
+class LoginFragment: Fragment(R.layout.fragment_login) {
+}
---
+++ app/src/main/java/com/example/mystore/fragments/loginRegister/RegisterFragment.kt
@@ -0,0 +1,7 @@
+package com.example.mystore.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.mystore.R
+
+class RegisterFragment: Fragment(R.layout.fragment_register) {
+}
