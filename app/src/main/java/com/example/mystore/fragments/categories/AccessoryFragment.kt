2cd2179d56aa17d8eacb15f29dc08246d27a742e
OSIELJ <osieljuniormartins@gmail.com>
2024-09-24 08:53:00 -0300

    Cirando para implementar menu de categorias na tela principal.

---
+++ app/src/main/java/com/example/mystore/fragments/categories/AccessoryFragment.kt
@@ -0,0 +1,4 @@
+package com.example.mystore.fragments.categories
+
+class AccessoryFragment: BaseCategoryFragment() {
+}
---
+++ app/src/main/java/com/example/mystore/fragments/categories/MainCategoryFragment.kt
@@ -0,0 +1,8 @@
+package com.example.mystore.fragments.categories
+
+import androidx.fragment.app.Fragment
+import com.example.mystore.R
+
+
+class MainCategoryFragment: Fragment(R.layout.fragment_main_category) {
+}
